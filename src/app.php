c7cffcfcd44a486fead6765c9f79d35c9fab5ecc
tallify <eliott@champau.lt>
2023-01-11 18:28:19 +0100

    v0.1.14

--- src/app.php
+++ src/app.php
@@ -20,7 +20,7 @@ if (file_exists(__DIR__ . '/../../../autoload.php')) {
 /**
  * Create the application.
  */
-$version = '0.1.13';
+$version = '0.1.14';
 
 $app = new Application('Tallify installer', $version);
 
